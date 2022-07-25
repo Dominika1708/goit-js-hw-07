14e4717f8882c205eb0d2510bfecfe1e9e91e0ca
Dominika Sosnowska <domi.sosnowska@o2.pl>
2022-07-25 10:42:32 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+    src = "${item.preview}"
+    data-source = "${item.original}"
+    alt = "${item.description}"/>
+    </a></div>`
+  )
+  .join("");
+
+gallery.innerHTML = galleryItem;
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  const selectedImg = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${selectedImg}" width="800" height="600">
+`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", (e) => {
+          if (e.key === "Escape") instance.close();
+        });
+      },
+    }
+  );
+
+  instance.show();
+});
